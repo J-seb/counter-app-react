35be030a1d441fd844818d8272c763be75e8d286
Sebastian Narvaez <juan.sebastian4991@gmail.com>
2021-06-15 13:53:36 -0500

    Add new Features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Counters from "./components/counters";
+import NavBar from "./components/navbar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    arrayOfCounters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 100 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  handleDelete(counterId) {
+    const newArrayOfCounters = this.state.arrayOfCounters.filter(
+      (counter) => counter.id !== counterId
+    );
+    this.setState({
+      arrayOfCounters: newArrayOfCounters,
+    });
+  }
+
+  handleIncrement(counter) {
+    const newArray = [...this.state.arrayOfCounters];
+    const index = newArray.findIndex((c) => c.id === counter.id);
+    newArray[index].value += 1;
+
+    this.setState({
+      arrayOfCounters: newArray,
+    });
+  }
+
+  handleDecrement(counter) {
+    const newArray = [...this.state.arrayOfCounters];
+    const index = newArray.findIndex((c) => c.id === counter.id);
+
+    newArray[index].value = !newArray[index].value
+      ? 0
+      : (newArray[index].value -= 1);
+
+    this.setState({
+      arrayOfCounters: newArray,
+    });
+  }
+
+  handleReset() {
+    const newArray = this.state.arrayOfCounters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+
+    this.setState({ arrayOfCounters: newArray });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalOfCounters={
+            this.state.arrayOfCounters.filter((c) => c.value > 0).length
+          }
+        />
+        <div className="container">
+          <Counters
+            counters={this.state.arrayOfCounters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = ({ totalOfCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Counters
+          <span className="badge rounded-pill bg-secondary m-2">
+            {totalOfCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
