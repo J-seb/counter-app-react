44a3e50265aff914cbc1b8e820f92ae59550ac3b
Sebastian Narvaez <juan.sebastian4991@gmail.com>
2021-06-18 11:41:39 -0500

    Fix details

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ const NavBar = ({ totalOfCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="./">
           Counters
           <span className="badge rounded-pill bg-secondary m-2">
             {totalOfCounters}
